6edb750595fb01fb86a5043fc85dc50a66f5eb68
Michael Stone <mikepwns37@gmail.com>
2018-01-24 20:15:08 -0500

    Add comments to marathon.js

--- marathon.js
+++ marathon.js
@@ -1,10 +1,14 @@
+// Sample user input. Subject to change depending on what values want to be used/ tested
 var minutes = 40 ;
 var hilly = 'N';
 
+// Prompt user for their half-marathon time
 console.log("Enter your half-marathon time in minutes: ");
 
-console.log("Is it a hilly course?('Y' for 'yes', 'N' for 'no'): ");
+// Prompt user to see if the marathon is hilly or not
+console.log("Is it a hilly course? ('Y' for 'yes', 'N' for 'no'): ");
 
+// Display time for full marathon depending on if course is hilly or not
 if (hilly === 'y' || hilly === 'Y') {
 	minutes = (minutes * 2) + 30;
 	console.log("It will take approximately " + minutes + " minutes to run a marathon.\n");
